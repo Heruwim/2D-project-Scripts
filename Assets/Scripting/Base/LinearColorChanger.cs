4beab7280ebbe52b0ff150bfb9fa418b06f59697
Heruwim <heruwim25@gmail.com>
2023-02-16 12:19:23 +0200

    Linking components

---
+++ Assets/Scripting/Base/LinearColorChanger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearColorChanger : MonoBehaviour
+{
+    [HideInInspector]public SpriteRenderer Target;
+    [SerializeField] private float _duration;
+
+    void Update()
+    {
+        
+    }
+}
