c34f2f4369c525ac14c3e9d4f4b76cdd25c98c65
Heruwim <heruwim25@gmail.com>
2023-02-20 15:01:31 +0200

    Collision and collision with blocks

--- Assets/Scripting/Base/Player2D.cs
+++ Assets/Scripting/Base/Player2D.cs
@@ -1,8 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player2D : MonoBehaviour
 {
-    
+    [SerializeField] private UnityEvent _hit;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.gameObject.name);
+        if(collision.collider.TryGetComponent(out Block block))
+        {
+            _hit?.Invoke();
+        }
+    }
 }
